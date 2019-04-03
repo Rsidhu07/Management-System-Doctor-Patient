17e31b8ed5f1bfe950abcc7ec69d080eff47567b
Rsidhu07 <rajsidhuh07@gmail.com>
2019-04-03 14:41:57 +0530

    made some changes

--- client/pages/not-found.js
+++ client/pages/not-found.js
@@ -11,7 +11,19 @@ Template.notFound.onCreated(function(){
           
          Session.set('tempKittyUrl', kittyUrl);
         }
-      }); 
+      });
+      
+      HTTP.call( 'GET', 'https://api.thedogapi.com/v1/images/search', {}, function( error, response ) {
+        if ( error ) {
+          console.log( error );
+        } else {
+          const dogUrl =  response.data[0].url;
+      
+          console.log(dogUrl);
+          
+         Session.set('tempDogUrl', dogUrl);
+        }
+      });
     
 });
 
@@ -21,6 +33,44 @@ Template.notFound.helpers({
    'kittyApi': function(){
 
     return Session.get('tempKittyUrl');
-    }
+    },
+
+    'dogApi': function(){
+
+      return Session.get('tempDogUrl');
+      }
+
+});
+
+Template.notFound.events({
+
+  'click .getImageDog': function(){
+    return HTTP.call( 'GET', 'https://api.thedogapi.com/v1/images/search', {}, function( error, response ) {
+      if ( error ) {
+        console.log( error );
+      } else {
+        const dogUrl =  response.data[0].url;
+    
+        console.log(dogUrl);
+        
+       Session.set('tempDogUrl', dogUrl);
+      }
+    });
+  },
+
+  'click .getImageCat': function(){
+
+    return HTTP.call( 'GET', 'https://api.thecatapi.com/v1/images/search', {}, function( error, response ) {
+      if ( error ) {
+        console.log( error );
+      } else {
+        const kittyUrl =  response.data[0].url;
+    
+        console.log(kittyUrl);
+        
+       Session.set('tempKittyUrl', kittyUrl);
+      }
+    });
+  }
 
 });
